f7fbaf1827b9a3b20c88bd0180b505cdbbf56aa2
Kenji0707 <yugetilya.kunn@gmail.com>
2024-02-19 15:36:37 +0900

    [add]カラム生成
    DB設計に則りshopモデルにカラムを追加

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,8 @@
 class Shop < ApplicationRecord
+
+    validates :name, presence: true
+    validates :latitude, presence: true
+    validates :longitude, presence: true
+    validates :evaluation, presence: true
+
 end
--- db/migrate/20240217074529_create_shops.rb
+++ db/migrate/20240217074529_create_shops.rb
@@ -3,6 +3,10 @@ class CreateShops < ActiveRecord::Migration[7.0]
     create_table :shops do |t|
 
       t.timestamps
+      t.string :name
+      t.float :latitude
+      t.float :longitude
+      t.float :evaluation
     end
   end
 end
